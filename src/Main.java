6ecf41fd94d6270f8b757be2e780686a2701fe41
lipeng <lp02388651@alibaba-inc.com>
2026-06-08 17:00:43 +0800

    fix: 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import utils.GitPushUtil;
 
 public class Main {
+
+    public static String RepoName = "myProject/helloWorld";
+
     public static void main(String[] args) {
+        // 推送 helloWorld 项目`
+        push(Main.RepoName);
+    }
+
+    public static void push(String gitRepoName) {
         try {
-            GitPushUtil.push("myProject/reactLearn");
+            GitPushUtil.push(gitRepoName);
         } catch (Exception e) {
             System.err.println("推送失败: " + e.getMessage());
         }
