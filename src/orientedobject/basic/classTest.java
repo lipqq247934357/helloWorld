f840a49937a368d925d853d46376c2337878322c
lipeng <lp02388651@alibaba-inc.com>
2026-06-12 17:58:10 +0800

    feat: 1

--- src/orientedobject/basic/classTest.java
+++ src/orientedobject/basic/classTest.java
@@ -1,5 +1,29 @@
 package orientedobject.basic;
 
 public class classTest {
-    
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        phone.brand = "华为";
+        phone.price = 1999;
+        phone.color = "黑色";
+        phone.call();
+        phone.sendMessage();
+        System.out.println(phone.brand + " " + phone.price + " " + phone.color);
+    }
+}
+
+class Phone {
+
+    String brand;
+    int price;
+    String color;
+
+    public void call() {
+        System.out.println("打电话");
+    }
+
+    public void sendMessage() {
+        System.out.println("发短信");
+
+    }
 }
