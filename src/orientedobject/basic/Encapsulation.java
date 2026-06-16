bee67a613e7753ba2c9f318fd1c4db9a1a236de8
lipeng <lp02388651@alibaba-inc.com>
2026-06-16 16:12:27 +0800

    feat: 1

--- src/orientedobject/basic/Encapsulation.java
+++ src/orientedobject/basic/Encapsulation.java
@@ -3,5 +3,34 @@ package orientedobject.basic;
 public class Encapsulation {
     
     public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.setName("小狗");
+        System.out.println(animal.getName());
+        animal.eat();
+        // 直接访问私有属性会报错
+        // animal.name = "小狗";
+        // 缺省修饰符，本包内可以访问
+        animal.color = "黑色";
+        System.out.println(animal.color);
+    }
+}
+
+class Animal {
+
+    private String name;
+    String color;
+    public void eat() {
+        System.out.println("吃");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        if (name.length() > 0) {
+            this.name = name;
+        } else {
+            System.out.println("名字不能为空");
+        }
     }
 }
